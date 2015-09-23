1fd08b563b729df575a727175bf84425b8204041
Chris <cmbusse@comcast.net>
2015-09-23 19:21:19 -0400

    Changed to new mongolab account

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,5 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://dev:dev@ds051933.mongolab.com:51933/daycamp'
-  }
+    uri: 'mongodb://dev:dev@ds051543.mongolab.com:51543/daycamp'
 };
